53e61e5ee344aa6047bbadc4005a89101aee6fc4
Russell Klenk <contact@russellklenk.com>
2014-08-19 09:40:08 -0700

    Updated InputManager to use ll_input.

--- include/input.hpp
+++ include/input.hpp
@@ -11,94 +11,34 @@
 ////////////////*/
 #include "common.hpp"
 #include "platform.hpp"
+#include "ll_input.hpp"
 
 /*////////////////
 //  Data Types  //
 ////////////////*/
-/// @summary Define the maximum number of axes supported per-controller.
-#define MAX_CONTROLLER_AXES        8
-
-/// @summary Define the maximum number of buttons supported per-controller.
-#define MAX_CONTROLLER_BUTTONS    16
-
-/// @summary Define the maximum number of controllers supported by the implementation.
-#define MAX_CONTROLLERS            2
-
-/// @summary Stores a snapshot of the current mouse state.
-struct mouse_state_t
-{
-    float    X;             /// The x-coordinate of the mouse cursor (relative to client area.)
-    float    Y;             /// The y-coordinate of the mouse cursor (relative to client area.)
-    uint32_t Buttons;       /// Bit i is set if the corresponding button is down.
-    uint32_t ModifierState; /// See http://www.glfw.org/docs/latest/group__mods.html
-};
-
-/// @summary Stores a snapshot of the current key state.
-struct keyboard_state_t
-{
-    uint32_t ModifierState; /// See http://www.glfw.org/docs/latest/group__mods.html
-    uint32_t KeyStates[11]; /// Bit i is set if the corresponding key is down. Offset by 32.
-};
-
-/// @summary Stores a snapshot of the current controller state.
+/// @summary A helper structure to describe the state of a controller.
+/// Pointers point to internal memory and should not be freed.
 struct controller_state_t
 {
-    uint32_t Index;         /// The zero-based index of the controller.
-    uint32_t Connected;     /// Set to one if the controller is connected.
-    uint32_t AxisCount;     /// The number of axes supplied by the controller.
-    uint32_t ButtonCount;   /// The number of buttons supplied by the controller.
-    float    AxisValues[8]; /// The values read for each valid axis.
-    uint8_t  ButtonValues[16]; /// The values read for each valid button.
-};
-
-/// @summary Stores a snapshot of the input state for all input devices.
-class InputState
-{
-public:
-    mouse_state_t      Mouse;
-    keyboard_state_t   Keyboard;
-    controller_state_t Controllers[MAX_CONTROLLERS];
-
-public:
-    InputState(void);
-    InputState(InputState const &other);
-    ~InputState(void);
-
-public:
-    InputState& operator =(InputState const &other);
-
-public:
-    void Reset(void);
+    int                  ControllerId;  /// The system identifier of the controller.
+    bool                 IsAttached;    /// true if the controller is attached.
+    size_t               AxisCount;     /// The number of axis values provided.
+    size_t               ButtonCount;   /// The number of button values provided.
+    float const         *AxisValues;    /// The controller axis values.
+    unsigned char const *ButtonValues;  /// The controller button values.
 };
 
 /// @summary Maintains a snapshot of state for user input devices.
 class InputManager
 {
 private:
-    GLFWwindow         *MainWindow;           /// The window to which we're attached.
-    InputState          CurrentState;         /// Input state for the current tick.
-    InputState          PreviousState;        /// Input state from the previous tick.
-
-public:
-    float               MouseDeltaX;          /// Mouse delta from the previous tick.
-    float               MouseDeltaY;          /// Mouse delta from the previous tick.
-    float               MouseX;               /// Current mouse position, relative to client area.
-    float               MouseY;               /// Current mouse position, relative to client area.
-    uint32_t            Modifiers;            /// Modifier keys active this tick.
-    size_t              KeysDownCount;        /// Number of keys down this tick.
-    size_t              KeysPressedCount;     /// Number of keys pressed this tick.
-    size_t              KeysReleasedCount;    /// Number of keys released this tick.
-    size_t              ButtonsDownCount;     /// Number of mouse buttons down this tick.
-    size_t              ButtonsPressedCount;  /// Number of mouse buttons pressed this tick.
-    size_t              ButtonsReleasedCount; /// Number of mouse buttons released this tick.
-    size_t              ControllerCount;      /// Number of active controllers this tick.
-    uint32_t           *KeysDown;             /// Keys down this tick.
-    uint32_t           *KeysPressed;          /// Keys pressed this tick.
-    uint32_t           *KeysReleased;         /// Keys released this tick.
-    uint32_t           *ButtonsDown;          /// Mouse buttons down this tick.
-    uint32_t           *ButtonsPressed;       /// Mouse buttons pressed this tick.
-    uint32_t           *ButtonsReleased;      /// Mouse buttons released this tick.
-    controller_state_t *Controllers;          /// Attached controllers this tick.
+    GLFWwindow       *MainWindow;       /// The window to which we're attached.
+    input_snapshot_t  CurrentState;     /// Input state for the current tick.
+    input_snapshot_t  PreviousState;    /// Input state from the previous tick.
+    uint32_t          ConnectEvents;    /// Bit i set if controller index i was connected.
+    uint32_t          DisconnectEvents; /// Bit i set if controller index i was disconnected.
+    float             MouseDeltaX;      /// Mouse delta from the previous tick.
+    float             MouseDeltaY;      /// Mouse delta from the previous tick.
 
 public:
     InputManager(void);
@@ -115,25 +55,88 @@ public:
     void Shutdown(void);
 
 public:
-    /// @summary Pushes a mouse movement event to the input system.
-    /// @param x The current position of the mouse cursor, in pixels, relative
-    /// to the client area of the attached window.
-    /// @param y The current position of the mouse cursor, in pixels, relative
-    /// to the client area of the attached window.
-    void MouseMove(double x, double y);
+    /// @summary Retrieve the state of all input devices for the current tick.
+    /// @return The input device state for the current tick.
+    input_snapshot_t const* GetCurrentSnapshot(void)  const { return &CurrentState;  }
 
-    /// @summary Pushes a mouse button event to the input system.
-    /// @param button One of the GLFW_MOUSE_BUTTON_x identifiers.
-    /// @param action One of GLFW_PRESS or GLFW_RELEASE.
-    /// @param modifiers GLFW modifier bitflags.
-    void MouseButton(int button, int action, int modifiers);
+    /// @summary Retrieve the state of all input devices for the previous tick.
+    /// @return The input device state for the previous tick.
+    input_snapshot_t const* GetPreviousSnapshot(void) const { return &PreviousState; }
 
-    /// @summary Pushes a keyboard event to the input system.
-    /// @param key One of the GLFW_KEY_x identifiers.
-    /// @param scan The raw OS scancode associated with the key.
-    /// @param action One of GLFW_PRESS, GLFW_RELEASE, or GLFW_REPEAT.
-    /// @param modifiers GLFW modifier bitflags.
-    void KeyChange(int key, int scan, int action, int modifiers);
+    /// @summary Retrieve the window attached to this InputManager.
+    /// @return The attached window.
+    GLFWwindow* GetWindow(void) const { return MainWindow; }
+
+    /// @summary Retrieve the change in mouse position during the tick.
+    /// @return The change in mouse position along the x-axis.
+    float GetMouseDeltaX(void) const { return MouseDeltaX; }
+
+    /// @summary Retrieve the change in mouse position during the tick.
+    /// @return The change in mouse position along the y-axis.
+    float GetMouseDeltaY(void) const { return MouseDeltaY; }
+
+public:
+    /// @summary Determines whether a key is currently not pressed.
+    /// @param key The GLFW button ID to query.
+    /// @return true if the key is currently not pressed.
+    bool IsKeyUp(int key) const;
+
+    /// @summary Determines whether a key is currently down.
+    /// @param key The GLFW button ID to query.
+    /// @return true if the key is currently pressed.
+    bool IsKeyDown(int key) const;
+
+    /// @summary Determines whether a mouse button is currently not pressed.
+    /// @param button The GLFW button ID to query.
+    /// @return true if the mouse button is currently not pressed.
+    bool IsMouseButtonUp(int button) const;
+
+    /// @summary Determines whether a mouse button is currently down.
+    /// @param button The GLFW button ID to query.
+    /// @return true if the mouse button is currently pressed.
+    bool IsMouseButtonDown(int button) const;
+
+    /// @summary Determines whether a controller is currently connected.
+    /// @param id The GLFW joystick ID to query.
+    /// @return true of the specified controller is currently connected.
+    bool IsControllerConnected(int id) const;
+
+    /// @summary Determines whether a key was pressed this tick.
+    /// @param key The GLFW key ID to query.
+    /// @return true if the specified key was pressed.
+    bool WasKeyPressed(int key) const;
+
+    /// @summary Determines whether a key was released this tick.
+    /// @param key The GLFW key ID to query.
+    /// @return true if the specified key was released.
+    bool WasKeyReleased(int key) const;
+
+    /// @summary Determines whether a mouse button was pressed this tick.
+    /// @param button The GLFW button ID to query.
+    /// @return true if the specified mouse button was pressed.
+    bool WasMouseButtonPressed(int button) const;
+
+    /// @summary Determines whether a mouse button was released this tick.
+    /// @param button The GLFW button ID to query.
+    /// @return true if the specified mouse button was released.
+    bool WasMouseButtonReleased(int button) const;
+
+    /// @summary Determines whether a controller was connected this tick.
+    /// @param id The GLFW joystick ID to query.
+    /// @return true if the specified controller was connected this tick.
+    bool WasControllerConnected(int id) const;
+
+    /// @summary Determines whether a controller was disconnected this tick.
+    /// @param id The GLFW joystick ID to query.
+    /// @return true if the specified controller was disconnected this tick.
+    bool WasControllerDisconnected(int id) const;
+
+    /// @summary Retrieve the state of a controller. The state snapshots are
+    /// valid until the next call to InputManager::Update().
+    /// @param id The GLFW joystick identifier of the controller.
+    /// @param prev On return, stores the state of the controller on the previous tick.
+    /// @param curr On return, stores the state of the controller on the current tick.
+    void GetController(int id, controller_state_t *prev, controller_state_t *curr) const;
 
 public:
     /// @summary Updates the current input device state.
--- src/input.cpp
+++ src/input.cpp
@@ -12,108 +12,37 @@
 /*/////////////////
 //   Constants   //
 /////////////////*/
-#define MAX_KEY_CODES        (11U * 32U)
-#define MAX_MOUSE_BUTTONS    (32U)
 
 /*///////////////////////
 //   Local Functions   //
 ///////////////////////*/
+static uint32_t controller_bitmap(input_snapshot_t const *state)
+{
+    uint32_t bitmap = 0;
+    for (size_t i = 0; i < state->ControllerCount; ++i)
+    {
+        bitmap |= (1 << state->ControllerIds[i]);
+    }
+    return bitmap;
+}
 
 /*///////////////////////
 //  Public Functions   //
 ///////////////////////*/
-InputState::InputState(void)
-{
-    Reset();
-}
-
-InputState::InputState(InputState const &other)
-    :
-    Mouse(other.Mouse),
-    Keyboard(other.Keyboard)
-{
-    for (size_t i = 0; i < MAX_CONTROLLERS; ++i)
-    {
-        Controllers[i] = other.Controllers[i];
-    }
-}
-
-InputState::~InputState(void)
-{
-    /* empty */
-}
-
-InputState& InputState::operator =(InputState const &other)
-{
-    if (this != &other)
-    {
-        Mouse    = other.Mouse;
-        Keyboard = other.Keyboard;
-        for (size_t i = 0; i < MAX_CONTROLLERS; ++i)
-        {
-            Controllers[i] = other.Controllers[i];
-        }
-    }
-    return *this;
-}
-
-void InputState::Reset(void)
-{
-    memset(&Mouse,       0, sizeof(mouse_state_t));
-    memset(&Keyboard,    0, sizeof(keyboard_state_t));
-    memset(&Controllers, 0, sizeof(controller_state_t) * MAX_CONTROLLERS);
-}
-
 InputManager::InputManager(void)
     :
     MainWindow(NULL),
+    ConnectEvents(0),
+    DisconnectEvents(0),
     MouseDeltaX(0.0f),
-    MouseDeltaY(0.0f),
-    MouseX(0.0f),
-    MouseY(0.0f),
-    Modifiers(0),
-    KeysDownCount(0),
-    KeysPressedCount(0),
-    KeysReleasedCount(0),
-    ButtonsDownCount(0),
-    ButtonsPressedCount(0),
-    ButtonsReleasedCount(0),
-    ControllerCount(0),
-    KeysDown(NULL),
-    KeysPressed(NULL),
-    KeysReleased(NULL),
-    ButtonsDown(NULL),
-    ButtonsPressed(NULL),
-    ButtonsReleased(NULL),
-    Controllers(NULL)
+    MouseDeltaY(0.0f)
 {
-    KeysDown        = new uint32_t[MAX_KEY_CODES];
-    KeysPressed     = new uint32_t[MAX_KEY_CODES];
-    KeysReleased    = new uint32_t[MAX_KEY_CODES];
-    ButtonsDown     = new uint32_t[MAX_MOUSE_BUTTONS];
-    ButtonsPressed  = new uint32_t[MAX_MOUSE_BUTTONS];
-    ButtonsReleased = new uint32_t[MAX_MOUSE_BUTTONS];
-    Controllers     = new controller_state_t[MAX_CONTROLLERS];
+    /* empty */
 }
 
 InputManager::~InputManager(void)
 {
     Shutdown();
-    MainWindow = NULL;
-    delete[] Controllers;     Controllers     = NULL;
-    delete[] ButtonsReleased; ButtonsReleased = NULL;
-    delete[] ButtonsPressed;  ButtonsPressed  = NULL;
-    delete[] ButtonsDown;     ButtonsDown     = NULL;
-    delete[] KeysReleased;    KeysReleased    = NULL;
-    delete[] KeysPressed;     KeysPressed     = NULL;
-    delete[] KeysDown;        KeysDown        = NULL;
-    KeysDownCount        = 0;
-    KeysPressedCount     = 0;
-    KeysReleasedCount    = 0;
-    ButtonsDownCount     = 0;
-    ButtonsPressedCount  = 0;
-    ButtonsReleasedCount = 0;
-    ControllerCount      = 0;
 }
 
 
@@ -121,21 +50,13 @@ bool InputManager::Init(GLFWwindow *win)
 {
     if (win != NULL)
     {
-        MainWindow = win;
-        CurrentState.Reset();
-        PreviousState.Reset();
-        MouseDeltaX          = 0.0f;
-        MouseDeltaY          = 0.0f;
-        MouseX               = 0.0f;
-        MouseY               = 0.0f;
-        Modifiers            = 0;
-        KeysDownCount        = 0;
-        KeysPressedCount     = 0;
-        KeysReleasedCount    = 0;
-        ButtonsDownCount     = 0;
-        ButtonsPressedCount  = 0;
-        ButtonsReleasedCount = 0;
-        ControllerCount      = 0;
+        MainWindow  = win;
+        MouseDeltaX = 0.0f;
+        MouseDeltaY = 0.0f;
+        ConnectEvents = 0;
+        DisconnectEvents = 0;
+        memset(&CurrentState,  0, sizeof(input_snapshot_t));
+        memset(&PreviousState, 0, sizeof(input_snapshot_t));
         return true;
     }
     else return false;
@@ -146,136 +67,153 @@ void InputManager::Shutdown(void)
     /* empty */
 }
 
-void InputManager::MouseMove(double x, double y)
+bool InputManager::IsKeyUp(int key) const
 {
-    CurrentState.Mouse.X = float(x);
-    CurrentState.Mouse.Y = float(y);
+    size_t   word = (key - INPUT_KEY_OFFSET) >> 5;
+    uint32_t bitx = (key - INPUT_KEY_OFFSET) & 0x1F;
+    uint32_t mask = (1  << bitx);
+    return (CurrentState.KeyboardState[word] & mask) == 0;
 }
 
-void InputManager::MouseButton(int button, int action, int modifiers)
+bool InputManager::IsKeyDown(int key) const
 {
-    if (action == GLFW_PRESS)
-    {
-        CurrentState.Mouse.Buttons |= (1 << button);
-        CurrentState.Mouse.ModifierState = modifiers;
-    }
-    if (action == GLFW_RELEASE)
-    {
-        CurrentState.Mouse.Buttons &=~(1 << button);
-        CurrentState.Mouse.ModifierState = modifiers;
-    }
+    size_t   word = (key - INPUT_KEY_OFFSET) >> 5;
+    uint32_t bitx = (key - INPUT_KEY_OFFSET) & 0x1F;
+    uint32_t mask = (1  << bitx);
+    return (CurrentState.KeyboardState[word] & mask) != 0;
 }
 
-void InputManager::KeyChange(int key, int scancode, int action, int modifiers)
+bool InputManager::IsMouseButtonUp(int button) const
 {
-    UNUSED_ARG(scancode);
-    if (action == GLFW_PRESS)
+    uint32_t mask = (1 << button);
+    return (CurrentState.MouseState & mask) == 0;
+}
+
+bool InputManager::IsMouseButtonDown(int button) const
+{
+    uint32_t mask = (1 << button);
+    return (CurrentState.MouseState & mask) != 0;
+}
+
+bool InputManager::IsControllerConnected(int id) const
+{
+    int const *ids = CurrentState.ControllerIds;
+    for (size_t  i = 0; i < CurrentState.ControllerCount; ++i)
     {
-        CurrentState.Keyboard.KeyStates[key >> 5] |= (1 << (key & 0x1F));
-        CurrentState.Keyboard.ModifierState = modifiers;
+        if (ids[i] == id)
+            return true;
     }
-    if (action == GLFW_RELEASE)
+    return false;
+}
+
+bool InputManager::WasKeyPressed(int key) const
+{
+    size_t   word    = (key - INPUT_KEY_OFFSET) >> 5;
+    uint32_t bitx    = (key - INPUT_KEY_OFFSET) & 0x1F;
+    uint32_t mask    = (1  << bitx);
+    uint32_t curr    = CurrentState.KeyboardState[word];
+    uint32_t prev    = PreviousState.KeyboardState[word];
+    uint32_t changes = (curr ^ prev);
+    return ((changes & curr) & mask) != 0;
+}
+
+bool InputManager::WasKeyReleased(int key) const
+{
+    size_t   word    = (key - INPUT_KEY_OFFSET) >> 5;
+    uint32_t bitx    = (key - INPUT_KEY_OFFSET) & 0x1F;
+    uint32_t mask    = (1  << bitx);
+    uint32_t curr    = CurrentState.KeyboardState[word];
+    uint32_t prev    = PreviousState.KeyboardState[word];
+    uint32_t changes = (curr  ^ prev);
+    return ((changes & ~curr) & mask) != 0;
+}
+
+bool InputManager::WasMouseButtonPressed(int button) const
+{
+    uint32_t mask    = (1 << button);
+    uint32_t curr    = CurrentState.MouseState;
+    uint32_t prev    = PreviousState.MouseState;
+    uint32_t changes = (curr ^ prev);
+    return ((changes & curr) & mask) != 0;
+}
+
+bool InputManager::WasMouseButtonReleased(int button) const
+{
+    uint32_t mask    = (1 << button);
+    uint32_t curr    = CurrentState.MouseState;
+    uint32_t prev    = PreviousState.MouseState;
+    uint32_t changes = (curr  ^ prev);
+    return ((changes & ~curr) & mask) != 0;
+}
+
+bool InputManager::WasControllerConnected(int id) const
+{
+    uint32_t mask = (1 << id);
+    return (ConnectEvents & mask) != 0;
+}
+
+bool InputManager::WasControllerDisconnected(int id) const
+{
+    uint32_t mask = (1 << id);
+    return (DisconnectEvents & mask) != 0;
+}
+
+void InputManager::GetController(int id, controller_state_t *prev, controller_state_t *curr) const
+{
+    curr->ControllerId = id;
+    curr->IsAttached   = false;
+    curr->AxisCount    = 0;
+    curr->ButtonCount  = 0;
+    curr->AxisValues   = NULL;
+    curr->ButtonValues = NULL;
+    for (size_t i = 0; i < CurrentState.ControllerCount; ++i)
     {
-        CurrentState.Keyboard.KeyStates[key >> 5] |= (1 << (key & 0x1F));
-        CurrentState.Keyboard.ModifierState = modifiers;
+        if (CurrentState.ControllerIds[i] == id)
+        {
+            curr->IsAttached   = true;
+            curr->AxisCount    = CurrentState.ControllerAxisCount[i];
+            curr->ButtonCount  = CurrentState.ControllerButtonCount[i];
+            curr->AxisValues   = CurrentState.ControllerAxes[i];
+            curr->ButtonValues = CurrentState.ControllerButtons[i];
+        }
+    }
+
+    prev->ControllerId = id;
+    prev->IsAttached   = false;
+    prev->AxisCount    = 0;
+    prev->ButtonCount  = 0;
+    prev->AxisValues   = NULL;
+    prev->ButtonValues = NULL;
+    for (size_t i = 0; i < PreviousState.ControllerCount; ++i)
+    {
+        if (PreviousState.ControllerIds[i] == id)
+        {
+            prev->IsAttached   = true;
+            prev->AxisCount    = PreviousState.ControllerAxisCount[i];
+            prev->ButtonCount  = PreviousState.ControllerButtonCount[i];
+            prev->AxisValues   = PreviousState.ControllerAxes[i];
+            prev->ButtonValues = PreviousState.ControllerButtons[i];
+        }
     }
 }
 
 void InputManager::Update(double currentTime, double elapsedTime)
 {
-    uint32_t curr    = 0;
-    uint32_t prev    = 0;
-    uint32_t changes = 0;
-    uint32_t downs   = 0;
-    uint32_t ups     = 0;
-    uint32_t mask    = 0;
-
     UNUSED_ARG(currentTime);
     UNUSED_ARG(elapsedTime);
 
-    // update the current mouse state.
-    MouseX      = CurrentState.Mouse.X;
-    MouseY      = CurrentState.Mouse.Y;
-    MouseDeltaX = CurrentState.Mouse.X - PreviousState.Mouse.X;
-    MouseDeltaY = CurrentState.Mouse.Y - PreviousState.Mouse.Y;
-    ButtonsDownCount     = 0;
-    ButtonsPressedCount  = 0;
-    ButtonsReleasedCount = 0;
-    curr    = CurrentState.Mouse.Buttons;
-    prev    = PreviousState.Mouse.Buttons;
-    changes = (curr    ^  prev);
-    downs   = (changes &  curr);
-    ups     = (changes & ~curr);
-    for (size_t j = 0; j < 32; ++j)
-    {
-        mask = (1 << j);
-        if (curr  & mask) ButtonsDown[ButtonsDownCount++] = j;
-        if (downs & mask) ButtonsPressed[ButtonsPressedCount++] = j;
-        if (ups   & mask) ButtonsReleased[ButtonsReleasedCount++] = j;
-    }
+    // current state becomes previous state, and the new current state is retrieved.
+    memcpy(&PreviousState, &CurrentState, sizeof(input_snapshot_t));
+    input_snapshot(&CurrentState, MainWindow);
+    MouseDeltaX = CurrentState.MouseX - PreviousState.MouseX;
+    MouseDeltaY = CurrentState.MouseY - PreviousState.MouseY;
 
-    // generate events for the keyboard:
-    KeysDownCount     = 0;
-    KeysPressedCount  = 0;
-    KeysReleasedCount = 0;
-    for (size_t i = 0; i < 11; ++i)
-    {
-        curr = CurrentState.Keyboard.KeyStates[i];
-        prev = PreviousState.Keyboard.KeyStates[i];
-        changes = (curr    ^  prev);
-        downs   = (changes &  curr);
-        ups     = (changes & ~curr);
-
-        for (size_t j = 0; j < 32; ++j)
-        {
-            uint32_t key = (i << 5) + j;
-            mask = (1 << j);
-            if (curr  & mask) KeysDown[KeysDownCount++] = key;
-            if (downs & mask) KeysPressed[KeysPressedCount++] = key;
-            if (ups   & mask) KeysReleased[KeysReleasedCount++] = key;
-        }
-    }
-
-    // poll joystick state.
-    ControllerCount = 0;
-    memset(CurrentState.Controllers, 0, sizeof(controller_state_t) * MAX_CONTROLLERS);
-    for (int i = GLFW_JOYSTICK_1; i <= GLFW_JOYSTICK_LAST; ++i)
-    {
-        if (glfwJoystickPresent(i) == GL_TRUE && ControllerCount < MAX_CONTROLLERS)
-        {
-            int           naxes    = 0;
-            float const   *axes    = glfwGetJoystickAxes(i, &naxes);
-            int           nbuttons = 0;
-            uint8_t const *buttons = glfwGetJoystickButtons(i, &nbuttons);
-            controller_state_t &cs = CurrentState.Controllers[ControllerCount];
-
-            if (naxes > MAX_CONTROLLER_AXES)
-                naxes = MAX_CONTROLLER_AXES;
-
-            if (nbuttons > MAX_CONTROLLER_BUTTONS)
-                nbuttons = MAX_CONTROLLER_BUTTONS;
-
-            cs.Index        = uint32_t(i);
-            cs.Connected    = 1;
-            cs.AxisCount    = uint32_t(naxes);
-            cs.ButtonCount  = uint32_t(nbuttons);
-            for (uint32_t j = 0; j < cs.AxisCount; ++j)
-            {
-                cs.AxisValues[j] = axes[j];
-            }
-            for (uint32_t j = 0; j < cs.ButtonCount; ++j)
-            {
-                cs.ButtonValues[j] = buttons[j];
-            }
-
-            Controllers[ControllerCount++] = cs;
-            if (ControllerCount == MAX_CONTROLLERS)
-            {
-                break;
-            }
-        }
-    }
-
-    // copy current state to the previous state.
-    PreviousState = CurrentState;
+    // determine whether any controllers have been connected or disconnected.
+    uint32_t curr       = controller_bitmap(&CurrentState);
+    uint32_t prev       = controller_bitmap(&PreviousState);
+    uint32_t changes    = (curr    ^  prev);
+    uint32_t connect    = (changes &  curr);
+    uint32_t disconnect = (changes & ~curr);
+    ConnectEvents       = connect;
+    DisconnectEvents    = disconnect;
 }
